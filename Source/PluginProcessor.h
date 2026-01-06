d91a74015d0adbff0a4e8c3f9be29ffbb4e68092
Nico <nicovieira.dev@gmail.com>
2026-01-06 10:07:59 +0000

    fix: add ducking between delay times

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -83,6 +83,15 @@ private:
     float lastHighCut = -1.0f;
 
     Tempo tempo;
+    
+    // Ducking
+    float delayInSamples = 0.0f;
+    float targetDelay = 0.0f;
+    float fade = 0.0f;
+    float fadeTarget = 0.0f;
+    float coefficient = 0.0f;
+    float wait = 0.0f;
+    float waitInc = 0.0f;      
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (GDelayAudioProcessor)
 };
